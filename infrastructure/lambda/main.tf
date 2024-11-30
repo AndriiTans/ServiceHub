d697b6a97175ee641aadd09eae14fc0f4a95d7c9
Andrii Tantsiura <andrew.tans9229@gmail.com>
2024-11-30 15:33:53 +0100

    Deployment

--- infrastructure/lambda/main.tf
+++ infrastructure/lambda/main.tf
@@ -1,5 +1,10 @@
+resource "random_string" "suffix" {
+  length  = 6
+  special = false
+}
+
 resource "aws_iam_role" "lambda_execution_role" {
-  name = "${var.function_name}_execution_role"
+  name = "${var.function_name}_execution_role_${random_string.suffix.id}"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
@@ -9,6 +14,10 @@ resource "aws_iam_role" "lambda_execution_role" {
       Principal = { Service = "lambda.amazonaws.com" }
     }]
   })
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 resource "aws_lambda_function" "this" {
