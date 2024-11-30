bae9bf319d4b6bd31913c1d050e43b5f580f419e
Andrii Tantsiura <andrew.tans9229@gmail.com>
2024-11-30 20:23:22 +0100

    Deployment

--- infrastructure/lambda/variables.tf
+++ infrastructure/lambda/variables.tf
@@ -19,3 +19,9 @@ variable "environment" {
   type        = map(string)
   default     = {}
 }
+
+variable "log_retention_days" {
+  description = "Number of days to retain CloudWatch logs"
+  type        = number
+  default     = 14
+}
