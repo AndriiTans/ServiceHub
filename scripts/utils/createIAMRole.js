70508e3477359967448fce74f2b34c56fda44aed
Andrii Tantsiura <andrew.tans9229@gmail.com>
2024-12-01 14:00:38 +0100

    Init

--- scripts/utils/createIAMRole.js
+++ scripts/utils/createIAMRole.js
@@ -1,7 +1,20 @@
 const { IAMClient, CreateRoleCommand } = require('@aws-sdk/client-iam');
 
-const createIAMRole = async (roleName, policyDocument) => {
+const createIAMRole = async (roleName) => {
   const client = new IAMClient({ region: 'us-east-1' });
+  const policyDocument = {
+    Version: '2012-10-17',
+    Statement: [
+      {
+        Effect: 'Allow',
+        Principal: {
+          Service: 'lambda.amazonaws.com',
+        },
+        Action: 'sts:AssumeRole',
+      },
+    ],
+  };
+
   const params = {
     RoleName: roleName,
     AssumeRolePolicyDocument: JSON.stringify(policyDocument),
@@ -13,9 +26,9 @@ const createIAMRole = async (roleName, policyDocument) => {
     console.log('IAM Role created:', response.Role.Arn);
     return response.Role.Arn;
   } catch (error) {
-    console.error('Error creating IAM Role:', error);
+    console.error('Error creating IAM Role:', error.message);
     throw error;
   }
 };
 
-module.exports = createIAMRole;
+module.exports = { createIAMRole };
